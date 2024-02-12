2ebb3e493cd1df58cd9df7f77dd8f79024296a09
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-02-12 21:30:21 +0400

    second commit'

---
+++ day3/day3.py
@@ -0,0 +1,38 @@
+teona = 38
+
+print(teona)
+
+shalva = 45
+
+print(shalva)
+
+barbare = 10
+
+print(barbare)
+
+teona = teona + 2
+
+print(teona)
+
+shalva = shalva + 2
+
+print(shalva)
+
+barbare = barbare + 2
+
+print(barbare)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
