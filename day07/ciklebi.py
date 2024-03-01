efd5bae47d46d2a0c270ceb2fa0d7051ff81d9e9
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-03-01 22:04:59 +0400

    second commit

---
+++ day07/ciklebi.py
@@ -0,0 +1,11 @@
+# shevqmenit str tipis cvladebi
+name = str(input("enter ur username: "))
+password = "goa sauketesoa"
+guess = input("enter password: ")
+
+
+while password != guess:
+    print("INCCORECT!")
+    guess = input("try again: ")
+
+print("correct!")
