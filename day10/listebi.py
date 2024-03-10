f91701019a418f3de6a55851b2c112d5a7975d49
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-03-10 16:01:57 +0400

    second commit

---
+++ day10/listebi.py
@@ -0,0 +1,31 @@
+programming_list = ["pyton", "javascript", "HTML", "C++", "gamedevelopment"]
+print(programming_list)
+print(programming_list[0])
+
+
+
+
+type_list = [1 , "house", 0.1,]
+print(type_list[0])
+print(type_list[1])
+print(type_list[2])
+
+num_list = [4, 8, 12, 16, 20]
+print(num_list[4])
+
+number_list = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+print(number_list[0])
+print(number_list[1])
+print(number_list[4])
+print(number_list[8])
+
+
+
+
+
+
+
+
+
+
+numb_list = [33, 39, 45]
