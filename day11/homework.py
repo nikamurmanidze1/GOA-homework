42ddfa78ffc5af879e809746fed4850463e49d4a
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-03-13 14:07:59 +0400

    second commit

---
+++ day11/homework.py
@@ -0,0 +1,28 @@
+print("this value is not found")
+print("list is soo long")
+print("i forgot to add parentheses")
+print("i always forgot commas ")
+print("this list includes vegetables only")
+print("i gave you lots of refers")
+print("i dont have acces to this site")
+print("You dont have permission to log in to you acc")
+print("You have alway been different from others")
+print("this code will output your name")
+print("list's are mutable ")
+print("string is immutable")
+print("i dont like taste of lime")
+print("This Trophy is on Display")
+print("theres sequences in alphabet")
+print("this will define you are staying here or not")
+
+
+
+
+
+
+
+
+
+
+
+
