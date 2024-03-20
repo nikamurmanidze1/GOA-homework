7575c25ebd1fc7e2d05a02725b11d6f5a25b20a0
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-03-20 18:28:52 +0400

    second commit

---
+++ day13/classwork/cw.py
@@ -0,0 +1,4 @@
+num_list = [1, 2, 3, 4, 5, 6]
+print(num_list[0:1])
+print(num_list[3:4])
+print(num_list[2:4])
---
+++ day13/homework/homework.py
@@ -0,0 +1,4 @@
+random_list = ["movies", "songs", "earth", "computer", "calcium"]
+
+for element in random_list:
+    print(element)
