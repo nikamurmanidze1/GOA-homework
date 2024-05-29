ea0596d2097f1f3bfb28ad88adf308f6a870d790
nikamurmanidze1 <nikamurmanidze2@gmail.com>
2024-05-29 14:10:07 +0400

    second commit

--- day34/homework/hw2.py
+++ day34/homework/hw2.py
@@ -1,16 +1,15 @@
 def square_digits(num):
-
-    num_str = str(num)
+    num_str = str(num) 
     squared_digits = []
-    
 
     for char in num_str:
         digit = int(char)
         squared_str = str(digit ** 2)
         squared_digits.append(squared_str)
-    
-
     concatenated_result = ''.join(squared_digits)
     result = int(concatenated_result)
+    
     return result
 
+
+print(square_digits(456))   
